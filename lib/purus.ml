8bd559d632fb5cad7b1970bbf2fdceb0a5b4cf8f
anqurvanillapy <anqurvanillapy@gmail.com>
2019-09-02 14:54:14 +0800

    Added command parsing

--- lib/purus.ml
+++ lib/purus.ml
@@ -2,11 +2,33 @@ module Ctx = Ctx
 module Abs = Abs
 module Typecheck = Typecheck
 
+let infile_name = ref None
+
+let specs = []
+
+let usage = "usage: purus [file]"
+
+let _ = Arg.parse specs (fun n -> infile_name := Some n) usage
+
 let run () =
-  let lexbuf = Lexing.from_channel stdin in
-  match Parser.top Lexer.read lexbuf with
-  | Some e ->
-      print_endline (Abs.show (Typecheck.type_of e)) ;
-      print_endline "Finished checking."
-  | None ->
-      exit 0
+  let fname, ic =
+    match !infile_name with
+    | Some f ->
+        (f, open_in_bin f)
+    | None ->
+        ("<stdin>", stdin)
+  in
+  let lexbuf = Lexing.from_channel ic in
+  try
+    match Parser.top Lexer.read lexbuf with
+    | Some e ->
+        print_endline (Abs.show (Typecheck.type_of e))
+    | None ->
+        exit 0
+  with e ->
+    let exn_name = Printexc.to_string e in
+    let p = Lexing.lexeme_start_p lexbuf in
+    Printf.fprintf stderr "%s:%d:%d: error: %s" fname p.Lexing.pos_lnum
+      (p.Lexing.pos_cnum - p.Lexing.pos_bol)
+      exn_name ;
+    exit 1
